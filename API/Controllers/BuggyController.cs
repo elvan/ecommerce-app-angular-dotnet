64018a4079d30c09b281215e7b45edb4f6b5e190
Elvan H <elvanhd@gmail.com>
2022-01-16 07:44:38 +0700

    creating a test controller for errors

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,46 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            Product thing = _context.Products.Find(42);
+
+            return thing == null ? NotFound() : Ok();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            Product thing = _context.Products.Find(42);
+
+            string thingToReturn = thing.ToString();
+
+            return Ok(thingToReturn);
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("validation-error")]
+        public ActionResult GetValidationError()
+        {
+            return Ok();
+        }
+    }
+}
