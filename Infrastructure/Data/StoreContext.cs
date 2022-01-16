00f6ff00c4e82073d3f29eb9e4478cdb11ea0529
Elvan H <elvanhd@gmail.com>
2022-01-16 14:35:28 +0700

    working around the decimal problem in sqlite

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Infrastructure.Data
 {
@@ -8,7 +11,6 @@ namespace Infrastructure.Data
     {
         public StoreContext(DbContextOptions<StoreContext> options) : base(options)
         {
-
         }
 
         public DbSet<Product> Products { get; set; }
@@ -21,6 +23,23 @@ namespace Infrastructure.Data
         {
             base.OnModelCreating(modelBuilder);
             _ = modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    IEnumerable<PropertyInfo> properties = entityType.ClrType
+                        .GetProperties()
+                        .Where(property => property.PropertyType == typeof(decimal));
+
+                    foreach (PropertyInfo property in properties)
+                    {
+                        _ = modelBuilder.Entity(entityType.Name)
+                            .Property(property.Name)
+                            .HasConversion<double>();
+                    }
+                }
+            }
         }
     }
 }
