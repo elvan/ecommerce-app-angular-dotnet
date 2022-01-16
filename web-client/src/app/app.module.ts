d7817a48ccdcfde8ecc5ae18ebe6ac87a4bb8f77
Elvan H <elvanhd@gmail.com>
2022-01-16 22:05:19 +0700

    using the http client module

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,7 +8,12 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [AppComponent, NavBarComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
