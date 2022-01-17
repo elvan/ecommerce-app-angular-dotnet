81477407cbf13ab4641a1fff85d2b13f422d2afd
Elvan H <elvanhd@gmail.com>
2022-01-17 14:00:50 +0700

    adding the pagination - template

--- web-client/src/app/shared/shared.module.ts
+++ web-client/src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, PaginationModule.forRoot()],
+  exports: [PaginationModule],
 })
 export class SharedModule {}
