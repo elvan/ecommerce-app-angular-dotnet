18a34ec01f176577b6d3c9c609ab045bb573a740
Elvan H <elvanhd@gmail.com>
2022-01-17 11:11:32 +0700

    adding the product filters

--- web-client/src/app/shop/shop.component.ts
+++ web-client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../shared/models/product';
+import { IProductBrand } from '../shared/models/product-brand';
+import { IProductType } from '../shared/models/product-type';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,13 +11,43 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[] = [];
+  brands: IProductBrand[] = [];
+  types: IProductType[] = [];
 
   constructor(private shopService: ShopService) {}
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getProductBrands();
+    this.getProductTypes();
+  }
+
+  getProducts() {
     this.shopService.getProducts().subscribe(
       (value) => {
         this.products = value.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getProductBrands() {
+    this.shopService.getProductBrands().subscribe(
+      (value) => {
+        this.brands = value;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getProductTypes() {
+    this.shopService.getProuctTypes().subscribe(
+      (value) => {
+        this.types = value;
       },
       (error) => {
         console.log(error);
--- web-client/src/app/shop/shop.service.ts
+++ web-client/src/app/shop/shop.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IPagination } from '../shared/models/pagination';
+import { IProductBrand } from '../shared/models/product-brand';
+import { IProductType } from '../shared/models/product-type';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +15,13 @@ export class ShopService {
 
   getProducts(): Observable<IPagination> {
     return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=10');
+  }
+
+  getProductBrands() {
+    return this.http.get<IProductBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getProuctTypes() {
+    return this.http.get<IProductType[]>(this.baseUrl + 'products/types');
   }
 }
