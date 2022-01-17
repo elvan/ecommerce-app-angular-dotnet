08fa5d3cd94b013776cc71fdc17932ea62252ebd
Elvan H <elvanhd@gmail.com>
2022-01-17 09:00:02 +0700

    organising the files and folders

---
+++ web-client/src/app/shop/shop.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ShopComponent } from './shop.component';
+
+@NgModule({
+  declarations: [ShopComponent],
+  imports: [CommonModule],
+})
+export class ShopModule {}
