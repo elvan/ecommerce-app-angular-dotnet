0bb3b717d1cdfa02c2811db5a62e09f021b93392
Elvan H <elvanhd@gmail.com>
2022-01-19 21:45:50 +0700

    creating additional components to route to

--- web-client/src/app/shop/shop.module.ts
+++ web-client/src/app/shop/shop.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { ShopComponent } from './shop.component';
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [CommonModule, SharedModule],
   exports: [ShopComponent],
 })
