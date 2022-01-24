c1adce45d59ae7a1252dfc62c0c9316bc3cca983
Elvan H <elvanhd@gmail.com>
2022-01-24 20:43:25 +0700

    creating an error component for testing the error responses

--- web-client/src/app/core/core.module.ts
+++ web-client/src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TestErrorComponent } from './test-error/test-error.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent],
   imports: [CommonModule, RouterModule],
   exports: [NavBarComponent],
 })
---
+++ web-client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss'],
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'products/00').subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getValidationError() {
+    this.http.get(this.baseUrl + 'products/invalid-id').subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
